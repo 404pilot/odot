fb8b2b4abf4785ff44e9c5f4060dac81bcaf8aeb
404pilot <404pilot@users.noreply.github.com>
2015-09-19 20:50:27 -0500

    added validations

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -19,7 +19,7 @@ class TodoItemsController < ApplicationController
       redirect_to todo_list_todo_items_path
     else
       flash[:error] = "There was a problem adding that todo list item"
-      redender action: :new
+      render action: :new
     end
   end
 
