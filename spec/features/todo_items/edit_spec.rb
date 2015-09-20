c7798ef11ea9083d7160b91c4a01617c6a99aa05
404pilot <404pilot@users.noreply.github.com>
2015-09-20 00:31:01 -0500

    added validation test for editing

--- spec/features/todo_items/edit_spec.rb
+++ spec/features/todo_items/edit_spec.rb
@@ -24,7 +24,43 @@ describe "Editing todo items" do
         expect(page).to have_content("Saved todo list item.")
 
         todo_item.reload
-        
+
         expect(todo_item.content).to eq("defaultUpdatedContentForTest")
+    end
+
+    it "is unsuccessfully with no content" do
+        visit_todo_list todo_list
+
+        within("#todo_item_#{todo_item.id}") do
+          click_link "Edit"
+        end
+
+        fill_in "Content", with: ""
+        click_button "Save"
+
+        expect(page).to_not have_content("Saved todo list item.")
+        expect(page).to have_content("Content can't be blank")
+
+        todo_item.reload
+
+        expect(todo_item.content).to eq("defaultContentForTest")
+    end
+
+    it "is unsuccessfully with not enough content" do
+        visit_todo_list todo_list
+
+        within("#todo_item_#{todo_item.id}") do
+          click_link "Edit"
+        end
+
+        fill_in "Content", with: ""
+        click_button "Save"
+
+        expect(page).to_not have_content("Saved todo list item.")
+        expect(page).to have_content("Content is too short")
+
+        todo_item.reload
+
+        expect(todo_item.content).to eq("defaultContentForTest")
     end
 end
