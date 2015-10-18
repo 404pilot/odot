da42b0b0afad235c5502d2dd3a7b49b85862584e
404pilot <404pilot@users.noreply.github.com>
2015-10-17 19:49:55 -0500

    more

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,4 +1,8 @@
 class TodoItem < ActiveRecord::Base
   validates :content, presence: true, length: { minimum: 2 }
   belongs_to :todo_list
+
+  def completed?
+    !self.completed_at.blank?
+  end
 end
